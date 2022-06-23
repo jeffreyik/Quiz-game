73912bcff3d5a3fa3e42e91cc0b184b4a9050cef
jeffreyik <ikechukwujeffrey19@gmail.com>
2022-06-23 09:37:08 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,130 @@
+import { listOfQuestions } from './questionsAPI.js';
+
+let questionContainer = document.querySelector(".question-container");
+let questionNumberEl = document.querySelector(".question-number");
+let questionPageEl = document.querySelector(".question-page");
+
+let questionEl = document.querySelector('.question-el h3');
+let optionEl = document.querySelectorAll('.option-el');
+
+let pointEl = document.querySelector(".point-el div");
+let playAgainBtn = document.querySelector(".play-again-btn");
+let correctAnswerEl = document.querySelector(".correct-answer");
+
+// Track current question
+let currentQuestionNumber = 1;
+let questionIndex = 0;
+
+// Points
+let quizPoint = 0;
+
+// Random Complements
+const complements = ["Nice Job", "Wohoo! You're doing great", "Awesome!", "Keep It Up", "Fantastic"];
+
+function getRandomComplement() {
+    let randomIndex = Math.floor(Math.random() * complements.length);
+    return complements[randomIndex];
+}
+
+console.log(getRandomComplement())
+
+function updateQuestionNumber() {
+    questionPageEl.textContent = `Question ${currentQuestionNumber} / ${listOfQuestions.length}`;
+    questionNumberEl.textContent = `Question ${currentQuestionNumber}`;
+}
+
+playAgainBtn.addEventListener('click', startGame);
+
+function startGame() {
+    currentQuestionNumber = 1;
+    questionIndex = 0;
+    quizPoint = 0;
+    questionContainer.style.display = "block"
+    nextBtn.style.display = "block";
+
+    pointEl.innerHTML = `Wohoo! Nice Game <br> you got ${quizPoint}/${5 * listOfQuestions.length}`;
+    pointEl.style.display = "none"
+    playAgainBtn.style.display = "none";
+    updateQuestionNumber();
+    renderQuestionsToUI();    
+}
+
+
+function renderQuestionsToUI() {
+    questionEl.textContent = listOfQuestions[questionIndex].question;
+    let { a, b, c, d } = listOfQuestions[questionIndex].options;
+
+    // Create option elements;
+    const optionsArray = [];
+
+    optionEl.forEach(option => {
+        optionsArray.push(option);
+        // Add click event listener to all options
+        
+    })
+    optionsArray[0].textContent = a;
+    optionsArray[1].textContent = b;
+    optionsArray[2].textContent = c;
+    optionsArray[3].textContent = d;
+}
+
+optionEl.forEach(option => {
+    option.addEventListener('click', () => {
+        let answer = listOfQuestions[questionIndex].answer
+        checkIfCorrect(option.textContent, listOfQuestions[questionIndex].options[answer], option);
+        setTimeout(() => {
+            moveToNextQuestion(option);
+        }, 1000);
+        
+    })
+});
+
+let nextBtn = document.querySelector('.next-question');
+nextBtn.addEventListener('click', moveToNextQuestion);
+    
+function moveToNextQuestion(option) {
+    if (currentQuestionNumber < listOfQuestions.length) {
+        currentQuestionNumber += 1;
+        questionIndex += 1;
+        // console.log(currentQuestionNumber);
+        renderQuestionsToUI();
+        updateQuestionNumber();
+    } else if (currentQuestionNumber === listOfQuestions.length) {
+        pointEl.innerHTML = `Wohoo! Nice Game <br> you got ${quizPoint}/${5 * listOfQuestions.length}`;
+        pointEl.style.display = "block"
+        playAgainBtn.style.display = "block";
+        questionContainer.style.display = "none"
+        nextBtn.style.display = "none";
+    }
+    else {
+        return;
+    }
+    option.id = "";
+    correctAnswerEl.textContent = "";
+        
+}
+
+function checkIfCorrect(selectedAnswer, correctAnswer, option) {
+    if (selectedAnswer === correctAnswer) {
+        // Add points if selected answer is correct
+        addPoint();
+        option.id = "correct";
+        correctAnswerEl.textContent = getRandomComplement();
+        correctAnswerEl.style.color = "springgreen";
+    } 
+    else {
+        option.id = "wrong";
+        correctAnswerEl.textContent = `Sorry the answer was: ${correctAnswer} :(`;
+        correctAnswerEl.style.color = "#ec311c";
+    }
+}
+
+function addPoint() {
+    if (currentQuestionNumber < (listOfQuestions.length + 1)) {
+        quizPoint += 5;
+        console.log(quizPoint);
+    }
+    
+}
+
+startGame();
---
+++ js/questionsAPI.js
@@ -0,0 +1,41 @@
+export const listOfQuestions = 
+[
+    { question: 'What is the full meaning of CSS?',
+      options: {
+          a: "Cascading Style Sheet",
+          b: "Computer Style Selector",
+          c: "Common System Structure",
+          d: "Code Style Structure"
+      },
+       answer: 'a'
+    },
+    { question: 'What is the full meaning of HTML?',
+      options: {
+          a: "None of the above",
+          b: "Hyperlink Text mark loaded",
+          c: "Hybrid Transfer model language",
+          d: "Hyper Text Markup Language"
+      },
+       answer: 'd'
+    },
+    {
+        question: "What is the full meaning of API?",
+        options: {
+            a: "Application Programmable Interface",
+            b: "Application Process Interface",
+            c: "Asynchronous Process Input",
+            d: "Application Programming Input"
+        },
+        answer: 'a'
+    },
+    {
+        question: "Which of these is not a CSS framework?",
+        options: {
+            a: "TailwindCSS",
+            b: "Bulma",
+            c: "React",
+            d: "Bootstrap"
+        },
+        answer: 'c'
+    },
+];
